c5e8906423a5c8e156a6799ec74e55c1573c32f1
trishinanton <74793031+trishinanton@users.noreply.github.com>
2021-07-27 23:25:33 +0300

    27.07.21

--- js/index.js
+++ js/index.js
@@ -22,10 +22,14 @@ menu.addEventListener('click',()=>{
     menu.classList.toggle('menu-active');
     if(menu.classList.contains('menu-active')){
         document.querySelector('.header__menu').innerHTML = 'Закрыть';
-        document.querySelector('.active-content').style.display = 'block'; 
+        document.querySelector('.active-content').style.display = 'block';
+        document.querySelector('.header__number').style.display = 'none';
+        document.querySelector('.header__button').style.display = 'none'; 
     }else{
         document.querySelector('.header__menu').innerHTML = 'Меню';
         document.querySelector('.active-content').style.display = 'none';
+        document.querySelector('.header__number').style.display = 'block';
+        document.querySelector('.header__button').style.display = 'block';  
     }
 });
 
@@ -54,3 +58,62 @@ arrServicesCard.map((el,i)=>{
        el.style.backgroundSize = '100% 100%'; 
     });
 });
+
+/* Инициализация слайдера "О компании"*/
+$('.test-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+/*Инициализация слайдера "Наши преимущества"  */
+$('.excellence__description').slick({
+    dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+document.querySelector('.excellence__description .slick-prev').innerHTML='❮'
+document.querySelector('.excellence__description .slick-next').innerHTML='❯'
+
+/* Счетчик слайдеров */  
+$(".test-slider").on("afterChange", function(event, slick, currentSlide, nextSlide){
+    $(".sliders__number-current").text(currentSlide + 1);
+});
+let totalSlides = $('.test-slider').slick("getSlick").slideCount
+document.querySelector('.sliders__number-total').innerHTML=totalSlides
+
+/* Логика кнопок */
+document.querySelector('.slick-prev').classList.add('sliders-button__left','button-slide');
+document.querySelector('.slick-prev').innerHTML='&lt';
+
+document.querySelector('.slick-next').innerHTML='&gt';
+document.querySelector('.slick-next').classList.add('sliders-button__right','button-slide');
+
+/* Плавный скролл */
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+
+/* Фиксированное меню */
+$(document).ready(function($) {
+    var g_top = 0;
+    $(window).scroll(function() {
+      var top = $(this).scrollTop();
+      
+      if ( top > g_top ) {
+        $('.header').fadeOut(400);
+      } else {
+        $('.header').fadeIn(400);
+      }
+      
+      g_top = top;    
+    });
+  });
