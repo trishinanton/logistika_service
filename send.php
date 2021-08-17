fd4292d223f3f58547ca579e355d368f8153ed61
trishinanton <tosha.trishin@bk.ru>
2021-08-17 21:39:56 +0300

    повиксил порт для отправки

--- send.php
+++ send.php
@@ -33,7 +33,7 @@ try {
     $mail->Username   = 'cool-dev@bk.ru'; // Логин на почте
     $mail->Password   = 'Trishin061195'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
+    $mail->Port       = 587;
     $mail->setFrom('cool-dev@bk.ru', 'Антон'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
@@ -41,34 +41,23 @@ try {
     $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
 
     // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }   
-}
+
 // Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
 $mail->Body = $body;    
 
 // Проверяем отравленность сообщения
-if ($mail->send()) {
-    return true;
+if (!$mail->send()) {
+    echo 'Error';
 } else {
-    return false;
+    header (location: "mail.php");
 }
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
+// } catch (Exception $e) {
+//     $result = "error";
+//     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+// }
 
-// Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-header (location: "index-cargo.html")
+// // Отображение результата
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+// // header (location: "mail.php")
