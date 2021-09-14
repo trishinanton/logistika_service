23a6fdefb28daf194a24413b79e0a68bcb9aff1e
MadMaxInsight <maxvarvarin3@gmail.com>
2021-09-14 18:47:01 +0300

    Настроил страницу благодарности

--- send.php
+++ send.php
@@ -1,63 +1,71 @@
 <?php
-// Файлы phpmailer
+session_start();
 require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
+if(isset($_POST['name']) && isset($_POST['tel'])){
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
+    // Проверка страницы с формой
+    $url_path = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH);
+    switch ($url_path) {
+        case '/index-tamojnya.html':
+            $page = 'Таможенное оформление';
+            break;
 
+        case '/index-cargo.html':
+            $page = 'Сборные грузы';
+            break;
+        case '/index-autsorsing.html':
+            $page = 'Аутсорсинг ВЭД';
+            break;
+        
+        default:
+            $page = 'Главная страница';
+            break;
+    }
 
-// Формирование самого письма
-$title = "Заголовок письма";
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Сообщение:</b><br>$tel
-";
+    // Формирование самого письма
+    $title = "Заявка с сайта skgrouptrans.ru";
+    $body = "
+    <h2>Данные</h2>
+    <b>Страница с формой:</b> $page<br>
+    <b>Имя:</b> $name<br>
+    <b>Почта:</b> $email<br>
+    <b>Телефон:</b> $tel<br>
+    ";
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
+    $_SESSION['error'] = 0;
+
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
     $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'cool-dev@bk.ru'; // Логин на почте
-    $mail->Password   = 'Trishin061195'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 587;
-    $mail->setFrom('cool-dev@bk.ru', 'Антон'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('site-notification@skgrouptrans.ru', 'SK Group Trans'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('tosha.trishin@bk.ru');  
-    $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+    $mail->addAddress('skgrouptrans@gmail.com');  
+    // $mail->addAddress('maxvarvarin3@gmail.com'); 
 
-    // Прикрипление файлов к письму
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
 
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    // Проверяем отравленность сообщения
 
-// Проверяем отравленность сообщения
-if (!$mail->send()) {
-    echo 'Error';
-} else {
-    header (location: "mail.php");
+    if (!$mail->send()) {
+        // неуданая отправка
+        $_SESSION['error'] = 1;
+        header ("Location: thank-you-page.php");
+    } else {
+        // удачная отправка
+        $_SESSION['error'] = 0;
+        header ("Location: thank-you-page.php");
+    }
+}else{    
+    $_SESSION['error'] = 1;
+    header ("Location: thank-you-page.php");
 }
-// } catch (Exception $e) {
-//     $result = "error";
-//     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-// }
-
-// // Отображение результата
-// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-// // header (location: "mail.php")
+?>
